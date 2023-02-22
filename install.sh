582e6ed7f04cf64084f2286669d52d8315f3999a
indirected <46082152+indirected@users.noreply.github.com>
2023-02-23 00:27:03 +0330

    force install python to 3.7

--- install.sh
+++ install.sh
@@ -9,30 +9,30 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if $MAKE_VENV; then
     # Virtualenv w/ python3
-    export PYTHONPATH=/usr/bin/python3 # point to your python3
-    python3 -m pip install virtualenv
-    cd $DIR
-    python3 -m virtualenv venv
+    export PYTHONPATH=/usr/bin/python3.7 # point to your python3
+    python3.7 -m pip install virtualenv
+    cd "$DIR"
+    python3.7 -m virtualenv venv
 fi
 
 if $SOURCE_VENV; then
-    cd $DIR
+    cd "$DIR"
     source venv/bin/activate
-    export PYTHONPATH=${DIR}/venv/bin/python/dist-packages
+    export PYTHONPATH="${DIR}/venv/bin/python/dist-packages"
 fi
 
 # Install this pkg and its requirements
-python -m pip install -e $DIR
+python3.7 -m pip install -e "$DIR"
 
 # Install RVO and its requirements
-cd $DIR/gym_collision_avoidance/envs/policies/Python-RVO2
-python -m pip install Cython
+cd "$DIR/gym_collision_avoidance/envs/policies/Python-RVO2"
+python3.7 -m pip install Cython
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.15
     brew install cmake || true
 fi
-python setup.py build
-python setup.py install
+python3.7 setup.py build
+python3.7 setup.py install
 
 # Install DRL Long's requirements
 # python -m pip install torch torchvision
